59e43722647430dc4ee775bf26ed4355f0ac7934
Darerb <daryae00@gmail.com>
2023-03-10 22:05:31 +0700

    1
    add hw1

---
+++ lesson1/lesson1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine(name + ", Hello!");
+        }
+    }
+}
