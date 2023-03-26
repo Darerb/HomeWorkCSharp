d1c3fc55f7c8ff82da59d2ec01d648c49d4eaf69
Darerb <daryae00@gmail.com>
2023-03-26 21:18:35 +0700

    HW3

---
+++ lesson3/lesson3/Ophthalmologist.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3
+{
+    public class Ophthalmologist : Doctor
+    {
+        public string Speciality { get; set; }
+        public Ophthalmologist (string doctor_name, int doctor_experience, string doctor_education) : base (doctor_name, doctor_experience, doctor_education)
+        {
+            Speciality = "ophthalmologist";
+            Name = doctor_name;
+            Experience = doctor_experience;
+            Education = doctor_education;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Специальность: офтальмолог, Имя: {Name}, Опыт: {Experience}, Образование: {Education}");
+        }
+        public override void Care(Patient patient)
+        {
+            if (patient.Status == false && Experience >= 4)
+            {
+                patient.Status = true;
+                Console.WriteLine("Сейчас полечим ваши глаза");
+            }
+            if (patient.Status == false && Experience < 4)
+            {
+                Console.WriteLine("Не знаю, как помочь вашим глазам :с");
+            }
+            else
+            {
+                Console.WriteLine("Со зрением все в порядке");
+            }
+        }
+    }
+}
---
+++ lesson3/lesson3/Therapist.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3
+{
+    public class Therapist : Doctor
+    {
+        public string Speciality { get; set; }
+        public Therapist (string doctor_name, int doctor_experience, string doctor_education) : base(doctor_name, doctor_experience, doctor_education)
+        {
+            Speciality = "therapist";
+            Name = doctor_name;
+            Experience = doctor_experience;
+            Education = doctor_education;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Специальность: терапевт, Имя: {Name}, Опыт: {Experience}, Образование: {Education}");
+        }
+        public override void Care(Patient patient)
+        {
+            if (patient.Status == false && Experience >= 1)
+            {
+                patient.Status = true;
+                Console.WriteLine("Сейчас полечим ваш кашель");
+            }
+            if (patient.Status == false && Experience < 1)
+            {
+                Console.WriteLine("Не знаю, как помочь с кашлем :с");
+            }
+            else
+            {
+                Console.WriteLine("Пациент не кашляет");
+            }
+        }
+    }
+}
+
