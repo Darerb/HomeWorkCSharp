6102738fad764011e82be19845e41a39321687f1
Darerb <daryae00@gmail.com>
2023-04-02 17:16:09 +0700

    HW4

---
+++ lesson4/lesson4/Counter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    public class Counter
+    {
+        public delegate void Counter50();
+
+        public event Counter50 Now50;
+
+        public void Count50()
+        { 
+            for (int i = 0; i<100; i++)
+            {
+               if (i == 50)
+               {
+                    Now50?.Invoke();
+               }
+            }
+        }
+    }
+}
---
+++ lesson4/lesson4/Handler2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    public class Handler2
+    {
+        public void Display()
+        {
+            Console.WriteLine($"Уже 50, стакан наполовину пуст");
+        }
+    }
+}
