b4439ec58a03f934b5c1b824de1a01372a376fce
Darerb <daryae00@gmail.com>
2023-04-09 16:45:21 +0700

    HW5

---
+++ lesson5_app1/lesson5_app1/FileManager.cs
@@ -0,0 +1,31 @@
+﻿namespace lesson5_app1;
+
+public static class FileManager
+{
+    public static List<DirectoryItem> GetDirectoryContents(string dirName)
+    {
+        var content = new List<DirectoryItem>();
+        var directories = Directory.GetDirectories(dirName);
+
+        foreach (var directory in directories)
+        {
+            content.Add(new DirectoryItem(
+                DirectoryItem.ItemTypes.Directory,
+                directory.Remove(0, dirName.Length + 1),
+                Directory.GetLastWriteTime(dirName)));
+        }
+        return content;
+    }
+
+    public static void ForceRecordToNewFile(string filePath, string content)
+    {
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+            Console.WriteLine($"Удален существующий файл");
+        }
+
+        using var writer = new StreamWriter(filePath);
+        writer.Write(content);
+    }
+}
---
+++ lesson5_app2/lesson5_app2/CsvManager.cs
@@ -0,0 +1,37 @@
+﻿namespace lesson5_app2;
+
+public static class CsvManager
+{
+    private const string Separator = "\"\\t\"";
+
+    public static List<DirectoryItem> GetItemsFromCsv(string pathToCsv)
+    {
+        var items = new List<DirectoryItem>();
+        var csv = File.ReadAllLines(pathToCsv);
+
+        foreach (var line in csv)
+        {
+            var tempStr = line.Split(Separator);
+            if (tempStr.Length != 3)
+
+            {
+                throw new FileLoadException($"Файл поврежден");
+            }
+
+            DirectoryItem.ItemTypes tempType;
+            switch (tempStr[0].Trim('"'))
+            {
+                case "File":
+                    tempType = DirectoryItem.ItemTypes.File;
+                    break;
+                case "Directory":
+                    tempType = DirectoryItem.ItemTypes.Directory;
+                    break;
+                default:
+                    throw new FileLoadException($"Файл поврежден");
+            }
+            items.Add(new DirectoryItem(tempType, tempStr[1], Convert.ToDateTime(tempStr[2].Trim('"'))));
+        }
+        return items;
+    }
+}
